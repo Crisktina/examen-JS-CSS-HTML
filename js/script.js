75b416d4270556d3b7d4c64d0fd41a3a06c51376
Crisktina <crishidalgolopez@gmail.com>
2024-06-06 14:04:17 +0200

    votar en porcentajes

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
-const boton = document.getElementById("addButton");
-boton.addEventListener("click", (e) => {
+//document.addEventListener('DOMContentLoaded', () => {
+// const buttons = document.querySelectorAll('button');
+// buttons.forEach(button => {
+//   button.addEventListener('click', (event) => {
+//     console.log('Ok.');
+//         event.preventDefault();
+//         console.log('Ok.');
+//     });
+// });
+//});
+
+//Añadir inputs en crear encuesta
+const botonAdd = document.getElementById("addButton");
+botonAdd.addEventListener("click", (e) => {
   e.preventDefault();
-  addOption(boton);
+  addOption(botonAdd);
 });
 
-function addOption(boton) {
+function addOption(botonAdd) {
   const opcionesEncuesta = document.getElementById("opcionesEncuesta");
 
   // Contar el número actual de inputs
@@ -22,6 +34,41 @@ function addOption(boton) {
   input.type = "text";
   input.placeholder = `Opción ${newOptionNumber}`;
   input.maxLength = "25";
+  //TODO: guardar el numero para luego la encuasta lo tenga en el value
 
-  opcionesEncuesta.insertBefore(input, boton);
+  opcionesEncuesta.insertBefore(input, botonAdd);
+}
+
+//TODO:crear encuesta a partir del form
+
+//survey boton votar
+let totalVotos = 0;
+
+btnNode = document.querySelectorAll(".btnSurvey");
+porcentajeNode = document.querySelectorAll(".porcentaje");
+//console.log(porcentajeNode[1].innerHTML);
+const votosXBtn = new Array(btnNode.length).fill(0);
+
+btnNode.forEach((button, index) => {
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
+    totalVotos++;
+    votosXBtn[index]++;
+    console.log(totalVotos + "total votos despues del click");
+    actualizarPorcentajes();
+  });
+});
+
+function calcularPorcentaje(votosOpcion) {
+  console.log(totalVotos + "totalvotos despues defuncion");
+  let porcentaje = (100 * votosOpcion) / totalVotos;
+  console.log(porcentaje + "calcularPorcentaje");
+  return porcentaje.toFixed(2);
+}
+function actualizarPorcentajes() {
+  porcentajeNode.forEach((p, index) => {
+    console.log(p.innerHTML);
+    p.innerHTML = calcularPorcentaje(votosXBtn[index]) + "%";
+    console.log(p.innerHTML);
+  });
 }
